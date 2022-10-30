5949110d7160b3fbbd4d2c699608f1ae6c144d54
caicai <1409262966@qq.com>
2022-10-30 20:03:52 +0800

    权限管理-功能键完成

--- src/views/permission/components/perDialog.vue
+++ src/views/permission/components/perDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 放置一个弹层 用来编辑新增节点 -->
-  <el-dialog title="新增权限点" :visible="showDialog" @close="btnCancel">
+  <el-dialog :title="isEdit ? '编辑权限点' : '新增权限点' " :visible="showDialog" @close="btnCancel">
     <!-- 表单 -->
     <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
       <el-form-item label="权限名称" prop="name">
@@ -43,9 +43,43 @@ export default {
     pid: {
       type: [String, Number],
       default: ''
+    },
+    isEdit: {
+      type: Boolean,
+      default: false
+    },
+    originList: {
+      type: Array,
+      default: (_) => []
     }
   },
   data() {
+    // 校验权限名称
+    const validName = (rule, value, callback) => {
+      // 添加时的校验： 名字不能取子元素的名字
+      let existNameList = this.originList.filter(item => item.pid === this.formData.pid)
+      if (this.isEdit) {
+        // 编辑时的校验： 名字不能取兄弟（排除自己）的名字
+        // 找兄弟，排除自己
+        existNameList = this.originList.filter(item => item.pid === this.formData.pid && item.id !== this.formData.id)
+      }
+      existNameList.map(it => it.name).includes(value)
+        ? callback(new Error(value + '已经占用'))
+        : callback()
+    }
+
+    // 校验权限标识
+    const validCode = (rule, value, callback) => {
+      // 添加时的校验：code不能重复
+      let existCodeList = this.originList
+      if (this.isEdit) {
+        // 编辑时的校验: code能取自己
+        existCodeList = this.originList.filter(item => item.id !== this.formData.id)
+      }
+      existCodeList.map(it => it.code).includes(value)
+        ? callback(new Error(value + '已经占用'))
+        : callback()
+    }
     return {
       permissionList: [], // 权限管理列表数据
       formData: {
@@ -57,8 +91,14 @@ export default {
         type: '' // 类型吖
       },
       rules: {
-        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
-        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+        name: [
+          { required: true, message: '权限名称不能为空', trigger: 'blur' },
+          { validator: validName, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '权限标识不能为空', trigger: 'blur' },
+          { validator: validCode, trigger: 'blur' }
+        ]
       }
     }
   },
@@ -70,6 +110,15 @@ export default {
   },
   methods: {
     btnCancel() {
+      this.formData = {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      }
+      this.$refs.perForm.resetFields()
       this.$emit('update:showDialog', false)
     },
     btnConfirm() {
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -13,21 +13,29 @@
           <el-table-column label="操作">
             <template #default="{ row }">
               <el-button v-if="row.type === 1" type="text" @click="addPerBtn(2, row.id)">添加</el-button>
-              <el-button type="text">编辑</el-button>
-              <el-button type="text">删除</el-button>
+              <el-button type="text" @click="editBtn(row.id)">编辑</el-button>
+              <el-button type="text" @click="deleteBtn(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
       </el-card>
 
       <!-- 添加按钮组件 -->
-      <per-dialog ref="perDialog" :show-dialog.sync="showDialog" :type="type" :pid="pid" @confirm="confirmFn" />
+      <per-dialog
+        ref="perDialog"
+        :show-dialog.sync="showDialog"
+        :type="type"
+        :pid="pid"
+        :is-edit="isEdit"
+        :origin-list="originList"
+        @confirm="confirmFn"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { getPermissionListAPI, addPermissionAPI } from '@/api'
+import { getPermissionListAPI, addPermissionAPI, getPermissionDetailAPI, updatePermissionAPI, delPermissionAPI } from '@/api'
 import { transTree } from '@/utils'
 import perDialog from './components/perDialog.vue'
 export default {
@@ -37,7 +45,9 @@ export default {
       permissionList: [], // 权限列表
       showDialog: false,
       type: '',
-      pid: ''
+      pid: '',
+      isEdit: false,
+      originList: [] // 在做校验时需要用到的数据
     }
   },
   created() {
@@ -48,21 +58,57 @@ export default {
     async getPermissionList() {
       const res = await getPermissionListAPI().catch(err => err)
       // console.log(res)
+      // 初始化 originList
+      this.originList = res.data
       this.permissionList = transTree(res.data, '0')
     },
+    // 添加权限
     addPerBtn(type, pid) {
+      this.isEdit = false
       this.showDialog = true
       this.type = type
       this.pid = pid
     },
+    // 确认添加-编辑/更新
     async confirmFn(formData) {
-      const res = await addPermissionAPI(formData)
-      if (!res.success) return this.$message.error(res.message)
+      if (this.isEdit) {
+        // 编辑-调用更新接口
+        const res = await updatePermissionAPI(formData).catch(err => err)
+        if (!res.success) return this.$message.error(res.message)
+        this.$message.success(res.message)
+      } else {
+        // 新增-调用新增接口
+        const res = await addPermissionAPI(formData)
+        if (!res.success) return this.$message.error(res.message)
+        this.$message.success(res.message)
+      }
       // 重新获取页面数据
       this.getPermissionList()
-      this.$message.success(res.message)
       this.$refs.perDialog.$refs.perForm.resetFields()
       this.showDialog = false
+    },
+    // 编辑按钮
+    async editBtn(id) {
+      this.isEdit = true
+      this.showDialog = true
+      // 数据回显
+      const res = await getPermissionDetailAPI(id)
+      // console.log(res)
+      this.$refs.perDialog.formData = res.data
+    },
+    // 删除按钮
+    async deleteBtn(id) {
+      const confirmRes = await this.$confirm('此操作将永久删除权限，是否删除？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      }).catch(err => err)
+      if (confirmRes === 'cancel') return this.$message.info('您取消了删除')
+
+      // 确认-调用接口
+      const res = await delPermissionAPI(id)
+      if (!res.success) return this.$message.error(res.message)
+      this.$message.success(res.message)
+      this.getPermissionList()
     }
   }
 }
